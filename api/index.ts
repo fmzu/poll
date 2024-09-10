c6b1f5bcae684ee2aec971a6d98fbb935f8fed9b
fmzu <fjtmzk0723@gmail.com>
2024-09-10 17:12:25 +0900

    refactor

--- api/index.ts
+++ api/index.ts
@@ -1,6 +1,6 @@
 import { WorkerEntrypoint } from "cloudflare:workers"
-import type { Env } from "./worker-configuration"
-import { app } from "./interface/api"
+import type { Env } from "~/worker-configuration"
+import { app } from "~/interface/api"
 
 export default class extends WorkerEntrypoint<Env> {
   async fetch(request: Request) {
